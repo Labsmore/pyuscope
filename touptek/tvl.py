5f8c08ab0dc74be461cba440821e503357cc5784
John McMaster <johndmcmaster@gmail.com>
2020-08-28 02:59:50 -0700

    tvl: cleanup
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- touptek/tvl.py
+++ touptek/tvl.py
@@ -28,6 +28,34 @@ Initialization constraints:
 
 from collections import OrderedDict
 
+prop_layout = OrderedDict([
+    ("Black balance", {
+        "bb-r",
+        "bb-g",
+        "bb-b",
+    }),
+    ("White balance", {
+        "wb-r",
+        "wb-g",
+        "wb-b",
+    }),
+    ("HSV+", {
+        "hue",
+        "saturation",
+        "brightness",
+        "contrast",
+        "gamma",
+    }),
+    ("Exposure", {
+        "auto-exposure",
+        "expotime",
+    }),
+    ("Flip", {
+        "hflip",
+        "vflip",
+    }),
+])
+
 class TestGUI(QMainWindow):
     def __init__(self, source=None):
         QMainWindow.__init__(self)
@@ -59,6 +87,51 @@ class TestGUI(QMainWindow):
             else:
                 assert 0, type(widget)
 
+    def assemble_gint(self, name, layoutg, row, ps):
+        def changed(name, value_label):
+            def f():
+                slider = self.ctrls[name]
+                try:
+                    val = int(slider.value())
+                except ValueError:
+                    pass
+                else:
+                    self.vidpip.source.set_property(name, val)
+                    value_label.setText(str(val))
+                    print('%s changed => %d' % (name, val))
+
+            return f
+
+        value_label = QLabel(str(ps.default_value))
+        layoutg.addWidget(QLabel(name), row, 0)
+        layoutg.addWidget(value_label, row, 1)
+        row += 1
+        slider = QSlider(Qt.Horizontal)
+        slider.setMinimum(ps.minimum)
+        slider.setMaximum(ps.maximum)
+        slider.setValue(ps.default_value)
+        slider.valueChanged.connect(changed(name, value_label))
+        self.ctrls[name] = slider
+        layoutg.addWidget(slider, row, 0, 1, 2)
+        row += 1
+        return row
+
+    def assemble_gboolean(self, name, layoutg, row, ps):
+        def changed(name, cb):
+            def f(val):
+                self.vidpip.source.set_property(name, val)
+                print('%s changed => %d' % (name, val))
+
+            return f
+
+        cb = QCheckBox(name)
+        cb.setChecked(ps.default_value)
+        cb.stateChanged.connect(changed(name, cb))
+        self.ctrls[name] = cb
+        layoutg.addWidget(cb, row, 0, 1, 2)
+        row += 1
+        return row
+
     def assemble_group(self, name, layoutg, row):
         self.properties.append(name)
         ps = self.vidpip.source.find_property(name)
@@ -69,46 +142,9 @@ class TestGUI(QMainWindow):
             print("%s, %s, default %s" % (name, ps.value_type.name, ps.default_value))
 
         if ps.value_type.name == "gint":
-            def changed(name, value_label):
-                def f():
-                    slider = self.ctrls[name]
-                    try:
-                        val = int(slider.value())
-                    except ValueError:
-                        pass
-                    else:
-                        self.vidpip.source.set_property(name, val)
-                        value_label.setText(str(val))
-                        print('%s changed => %d' % (name, val))
-
-                return f
-
-            value_label = QLabel(str(ps.default_value))
-            layoutg.addWidget(QLabel(name), row, 0)
-            layoutg.addWidget(value_label, row, 1)
-            row += 1
-            slider = QSlider(Qt.Horizontal)
-            slider.setMinimum(ps.minimum)
-            slider.setMaximum(ps.maximum)
-            slider.setValue(ps.default_value)
-            slider.valueChanged.connect(changed(name, value_label))
-            self.ctrls[name] = slider
-            layoutg.addWidget(slider, row, 0, 1, 2)
-            row += 1
+            row = self.assemble_gint(name, layoutg, row, ps)
         elif ps.value_type.name == "gboolean":
-            def changed(name, cb):
-                def f(val):
-                    self.vidpip.source.set_property(name, val)
-                    print('%s changed => %d' % (name, val))
-
-                return f
-
-            cb = QCheckBox(name)
-            cb.setChecked(ps.default_value)
-            cb.stateChanged.connect(changed(name, cb))
-            self.ctrls[name] = cb
-            layoutg.addWidget(cb, row, 0, 1, 2)
-            row += 1
+            row = self.assemble_gboolean(name, layoutg, row, ps)
         else:
             assert 0, ps.value_type.name
         return row
@@ -128,33 +164,6 @@ class TestGUI(QMainWindow):
             self.ctrls = {}
 
             self.properties = []
-            prop_layout = OrderedDict([
-                ("Black balance", {
-                    "bb-r",
-                    "bb-g",
-                    "bb-b",
-                }),
-                ("White balance", {
-                    "wb-r",
-                    "wb-g",
-                    "wb-b",
-                }),
-                ("HSV+", {
-                    "hue",
-                    "saturation",
-                    "brightness",
-                    "contrast",
-                    "gamma",
-                }),
-                ("Exposure", {
-                    "auto-exposure",
-                    "expotime",
-                }),
-                ("Flip", {
-                    "hflip",
-                    "vflip",
-                }),
-            ])
 
             for group_name, group in prop_layout.items():
                 groupbox = QGroupBox(group_name)

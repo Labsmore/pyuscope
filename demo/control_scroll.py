73f41d8429ea8e8afff5f7965e0933b7c1fdea12
John McMaster <johndmcmaster@gmail.com>
2020-08-29 00:20:13 -0700

    demo/control_scroll.py baseline
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

---
+++ demo/control_scroll.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Comprehensive image acquisition test GUI
+No motion control
+"""
+
+from uscope import gstwidget
+from uscope.touptek_util import TTControlScroll
+
+from uscope.gstwidget import GstVideoPipeline, gstwidget_main
+from uscope.gst_util import CbSink
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('GstBase', '1.0')
+gi.require_version('GstVideo', '1.0')
+
+from gi.repository import GstVideo
+
+from gi.repository import Gst
+Gst.init(None)
+from gi.repository import GstBase, GObject
+"""
+Initialization constraints:
+-Gst initialization needs
+"""
+
+import datetime
+import os
+
+class TestGUI(QMainWindow):
+    def __init__(self, source=None):
+        QMainWindow.__init__(self)
+        self.showMaximized()
+        self.vidpip = GstVideoPipeline(source=source)
+        self.vidpip.size_widgets(frac=0.5)
+
+        self.vidpip.setupGst()
+        self.initUI()
+        self.vidpip.run()
+
+    def initUI(self):
+        self.setWindowTitle('Test')
+        self.vidpip.setupWidgets()
+
+        layout = QHBoxLayout()
+        self.control_scroll = TTControlScroll(self.vidpip)
+        layout.addWidget(self.control_scroll)
+        layout.addWidget(self.vidpip.full_widget)
+
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+        self.show()
+
+
+if __name__ == '__main__':
+    gstwidget_main(TestGUI)

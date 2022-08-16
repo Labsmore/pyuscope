39663739bc62a619ded4ed56280094c0595dc30d
John McMaster <johndmcmaster@gmail.com>
2022-08-15 22:30:24 -0700

    3018 tui cleanup
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- test/cnc3018/jog_tui.py
+++ test/cnc3018/jog_tui.py
@@ -6,48 +6,50 @@ import time
 import sys
 from threading import Thread, Lock
 
-if len(sys.argv) == 2:
-    p = sys.argv[1]
-else:
-    p = '/dev/tty.usbserial-1410'
-
-s = serial.Serial(p, 115200)
-#s.open()
-mutex = Lock()
-
-
 def on_press(key):
     mutex.acquire()
+    value = getattr(key, "char", None)
     print("")
-    value = key.char
-    # value = key.value
-    print('alphanumeric key {0} pressed'.format(
-        value))
+    print('pressed %s' % value)
     if value == 'd':
         s.write(b'$J=G91 X1 F100\n')
+        s.flush()
     if value == 'a':
         s.write(b'$J=G91 X-1 F100\n')
+        s.flush()
     mutex.release()
 
 def on_release(key):
     mutex.acquire()
+    value = getattr(key, "char", None)
     print("")
-    s.write(b'\x85')
+    print('released %s' % value)
     # Adding delayed cancel as well, because
     # there are times when the cancel doesn't
     # take, possibly a queueing issue.
     # 0.1 doesn't seem to work. 0.2 is fine.
-    time.sleep(0.2)
-    s.write(b'\x85')
-    print('{0} released'.format(
-        key))
+    for i in range(3):
+        s.write(b'\x85')
+        s.flush()
+        time.sleep(0.1)
     if key == keyboard.Key.esc:
         # Stop listener
         return False
     mutex.release()
 
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Control X axis with a/d")
+    parser.add_argument("tty", nargs="?", default='/dev/tty.usbserial-1410', help="tty")
+    args = parser.parse_args()
+
+    s = serial.Serial(args.tty, 115200)
+    mutex = Lock()
+
+    # Collect events until released
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()

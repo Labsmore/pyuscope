b812b6289009f5ce3b87e5c2b60f7904878ebec8
John McMaster <johndmcmaster@gmail.com>
2022-09-01 02:57:45 -0700

    sm3590t config template script

---
+++ configs/lip-a1/sm3590t_template.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate objective template")
+    parser.add_argument("--scalar",
+                        type=float,
+                        required=True,
+                        help="Image width @ 4.5x w/ no barlow lens")
+    args = parser.parse_args()
+
+    ref_scalar = args.scalar
+    ref_mag = 4.5
+
+    for barlow_str in ("0.5", "1.0", "2.0"):
+        barlow_scalar = float(barlow_str)
+
+        for knob_str in ("0.7", "1.5", "3.0", "4.5"):
+            knob_scalar = float(knob_str)
+            entry_name = "B%sx @ %sx" % (barlow_str, knob_str)
+            this_mag = barlow_scalar * knob_scalar
+            x_view = ref_scalar * ref_mag / this_mag
+            print("""\
+        {
+            "name":"%s",
+            "x_view": %0.3f
+        },""" % (entry_name, x_view))

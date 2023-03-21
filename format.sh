2e40738a682c7911b34c0286f093169dc8f3fea8
John McMaster <johndmcmaster@gmail.com>
2023-03-21 12:42:14 -0700

    format.sh: parallel

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # find . -name '*.py' -and -not -path './.git/*' -and -not -path './archive/*' | xargs -0 -P $(nproc) yapf -p -i
-find . -name '*.py' -and -not -path './.git/*' -and -not -path './archive/*' -and -not -path './xystitch/*' -exec echo {} \; -exec yapf3 -p -i {} \;
+find . -name '*.py' -and -not -path './.git/*' -and -not -path './archive/*' -and -not -path './xystitch/*' -exec echo {} \; -exec yapf3 -p -i {} \+
 

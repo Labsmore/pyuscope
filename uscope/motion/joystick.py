89fb48fb3a62826143fc9ac1e82293c369de5270
labsmore <463376+yusufm@users.noreply.github.com>
2023-04-16 14:51:12 -0700

    Cleaning up joystick fns to prepare for getting from config file.

--- uscope/motion/joystick.py
+++ uscope/motion/joystick.py
@@ -16,31 +16,31 @@ class Joystick(object):
     def execute(self):
         # Get events and perform actions
         pygame.event.get()
-        self.set_jog_slider_value()
-        self.capture_image()
-        self.move_x()
-        self.move_y()
-        self.move_z()
+        self.axis_set_jog_slider_value(3)
+        self.btn_capture_image(0)
+        self.axis_move_x(0)
+        self.axis_move_y(1)
+        self.hat_move_z(0, 1)
 
     def _move(self, axis, val):
         self.parent.motion_thread.jog({axis: val})
 
-    def move_x(self):
-        val = self.joystick.get_axis(0)
+    def axis_move_x(self, id):
+        val = self.joystick.get_axis(id)
         if abs(val) < self.threshold:
             return
         val = math.copysign(self.parent.mainTab.motion_widget.slider.get_jog_val(), val)
         self._move('x', val)
 
-    def move_y(self):
-        val = self.joystick.get_axis(1)
+    def axis_move_y(self, id):
+        val = self.joystick.get_axis(id)
         if abs(val) < self.threshold:
             return
         val = math.copysign(self.parent.mainTab.motion_widget.slider.get_jog_val(), val)
         self._move('y', val)
 
-    def move_z(self):
-        val = self.joystick.get_hat(0)[1]
+    def hat_move_z(self, id, idx):
+        val = self.joystick.get_hat(id)[idx]
         # If hat is not pressed skip
         if val == 0:
             return
@@ -48,11 +48,11 @@ class Joystick(object):
         val = val * self.parent.mainTab.motion_widget.slider.get_jog_val()
         self._move('z', val)
 
-    def capture_image(self):
-        if self.joystick.get_button(0):
+    def btn_capture_image(self, id):
+        if self.joystick.get_button(id):
             self.parent.mainTab.snapshot_widget.take_snapshot()
 
-    def set_jog_slider_value(self, invert=True):
+    def axis_set_jog_slider_value(self, id, invert=True):
         # The min and max values of the joystick range (it is not
         # always -1 to 1)
         val_min = -1.0
@@ -61,7 +61,7 @@ class Joystick(object):
         # expects, which is 0 to 1.
         new_min = 0.0
         new_max = 1.0
-        val = -self.joystick.get_axis(3) if invert else self.joystick.get_axis(3)
+        val = -self.joystick.get_axis(id) if invert else self.joystick.get_axis(id)
         old_range = val_max - val_min
         new_range = new_max - new_min
         new_value = (((val - val_min) * new_range) / old_range) + new_min

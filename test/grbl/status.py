5a37b3c19c6a25fd5a38bda6d273b335be387938
John McMaster <johndmcmaster@gmail.com>
2023-05-13 16:09:58 -0700

    grbl status: print as config directives

--- test/grbl/status.py
+++ test/grbl/status.py
@@ -14,6 +14,35 @@ import threading
 import time
 
 
+def reformat_config(s):
+    """
+    "$0=10 (step pulse,usec)",
+    to
+    ("$0=10", "step pulse,usec")
+    """
+    s = s.strip()
+    if "(" in s:
+        config, comment = s.split("(")
+        comment = comment.replace(")", "").strip()
+        config = config.strip()
+        return config, comment
+    else:
+        return s, None
+
+
+def print_config(s, prefix=""):
+    config, comment = reformat_config(s)
+    if comment:
+        print(f'{prefix}"{config}", //{comment}')
+    else:
+        print(f'{prefix}"{config}",')
+
+
+def print_configs(l):
+    for s in l:
+        print_config(s, prefix="")
+
+
 def main():
     import argparse
 
@@ -21,27 +50,30 @@ def main():
     add_bool_arg(parser, "--verbose", default=False, help="Verbose output")
     args = parser.parse_args()
 
-    grbl = GRBL(verbose=args.verbose)
+    grbl = GRBL()
     print("Open ok")
-    # Can take up to two times to pop all status info
-    # Third print is stable
-    for i in range(3):
+    if args.verbose:
+        # Can take up to two times to pop all status info
+        # Third print is stable
+        for i in range(3):
+            print("")
+            print("? (%u / %u)" % (i + 1, 3))
+            print(grbl.gs.question())
         print("")
-        print("? (%u / %u)" % (i + 1, 3))
+        print("i")
+        print_configs(grbl.gs.i())
+        # FIXME
+        print("")
+        print("g")
+        print_config(grbl.gs.g())
+        print("")
+        print("$")
+        print_configs(grbl.gs.dollar())
+        print("")
+        print("#")
+        print_configs(grbl.gs.hash())
+    else:
         print(grbl.gs.question())
-    print("")
-    print("i")
-    print(grbl.gs.i())
-    # FIXME
-    print("")
-    print("g")
-    print(grbl.gs.g())
-    print("")
-    print("$")
-    print(grbl.gs.dollar())
-    print("")
-    print("#")
-    print(grbl.gs.hash())
 
 
 if __name__ == "__main__":

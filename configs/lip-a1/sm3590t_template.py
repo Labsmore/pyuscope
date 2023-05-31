d2617a3d065cbec6e35a6d013b4f1d2611862142
John McMaster <johndmcmaster@gmail.com>
2023-05-30 23:08:42 -0700

    a1: add mag
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- configs/lip-a1/sm3590t_template.py
+++ configs/lip-a1/sm3590t_template.py
@@ -18,11 +18,13 @@ if __name__ == "__main__":
 
         for knob_str in ("0.7", "1.5", "3.0", "4.5"):
             knob_scalar = float(knob_str)
+            mag = barlow_scalar * knob_scalar
             entry_name = "B%sx @ %sx" % (barlow_str, knob_str)
             this_mag = barlow_scalar * knob_scalar
             x_view = ref_scalar * ref_mag / this_mag
             print("""\
-        {
-            "name":"%s",
-            "x_view": %0.3f
-        },""" % (entry_name, x_view))
+                {
+                    "name":"%s",
+                    "magnification": %0.3f,
+                    "x_view": %0.3f
+                },""" % (entry_name, mag, x_view))

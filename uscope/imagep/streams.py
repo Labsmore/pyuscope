225264ee4a2e1771cb44df069e0969fd93cc8e1d
John McMaster <johndmcmaster@gmail.com>
2024-01-09 23:06:15 -0800

    profiling: bug fix

--- uscope/imagep/streams.py
+++ uscope/imagep/streams.py
@@ -475,8 +475,8 @@ class SnapshotCSIP:
             self.verbose and self.log("FF correction: start")
             tb = TaskBarrier()
             data_out = self.csip.queue_correct_ff1(im_in=current_image,
-                                        want_im_out=True,
-                                        tb=tb)
+                                                   want_im_out=True,
+                                                   tb=tb)
             tb.wait()
             current_image = data_out["image"].get_im()
 

7e88aebc2d5e49768ef33466d38c11b5cb497f15
John McMaster <johndmcmaster@gmail.com>
2024-01-23 00:02:38 -0800

    setup script: ubuntu 22.04 support, fewer error messages

--- setup_ubuntu_20.04.sh
+++ setup_ubuntu_20.04.sh
@@ -7,28 +7,34 @@ if [ \! -d configs ] ; then
     echo "Must be run from the root dir"
     exit 1
 fi
+linux_distribution=$(lsb_release -d |cut -d ':' -f2 |xargs)
 
 sudo apt-get update
 sudo apt-get install -y python3-pip
 
 install_pyuscope() {
-    sudo apt-get install -y python3-gst-1.0 python3-gi python3-pyqt5 python3-usb python3-opencv python3-serial python3-numpy python3-scipy imagemagick enfuse python3-distro
+    sudo apt-get install -y python3-gst-1.0 python3-gi python3-pyqt5 python3-usb python3-opencv python3-serial python3-numpy python3-scipy imagemagick python3-distro
     # 2024-01-03: dev seems to be required, not just base
     # Some people suggest gir1.2-gst-rtsp-server-1.0
     # Install all for now
     sudo apt-get install -y libgstrtspserver-1.0-0 libgstrtspserver-1.0-dev gir1.2-gst-rtsp-server-1.0
-    sudo pip3 install json5 boto3 pygame psutil bitarray
+    pip3 install --user json5 boto3 pygame psutil bitarray
 
 
-    # Ubuntu 20.04
-    sudo apt-get install -y hugin-tools
-    # Ubuntu 22.04
-    # sudo apt update && sudo apt install flatpak
-    # flatpak install https://dl.flathub.org/repo/appstream/net.sourceforge.Hugin.flatpakref
+    # Package removed
+    # use flat pack
+    if ["$linux_distribution" == "Linux Ubuntu 22.04"] ; then
+        sudo apt update && sudo apt install flatpak
+        flatpak install https://dl.flathub.org/repo/appstream/net.sourceforge.Hugin.flatpakref
+    # Linux Ubuntu 20.04
+    # Linux Mint 20.3
+    else
+        sudo apt-get install -y hugin-tools enfuse
+    fi
 
     # For webserver
     sudo apt-get install -y python3-werkzeug
-    sudo pip3 install Flask>=2.2.2
+    pip3 install --user Flask>=2.2.2
 
     sudo python3 setup.py develop
 }
@@ -46,7 +52,7 @@ install_antmicro_v4l2() {
 }
 
 install_pyrav4l2() {
-    sudo pip3 install git+https://github.com/antmicro/pyrav4l2.git
+    pip3 install --user git+https://github.com/antmicro/pyrav4l2.git
 }
 
 # For GRBL etc serial port
@@ -61,8 +67,9 @@ install_gst_plugin_toupcam() {
 }
 
 apply_migrations() {
-    # User might not have microscope attached
-    ./test/grbl/migrate_meta.py || true
+    # User might not have microscope attached, not homed, et
+    # Make token effort though
+    ./test/grbl/migrate_meta.py 2>/dev/null || true
 }
 
 install_gst_plugin_toupcam

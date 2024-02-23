4c782c0c1d01e29df51a52b767c1256f1b956e0b
John McMaster <johndmcmaster@gmail.com>
2024-02-22 19:54:12 -0800

    web api: clean up decorators

--- uscope/script/webserver_common.py
+++ uscope/script/webserver_common.py
@@ -36,214 +36,166 @@ def make_app(app):
         }
 
     @app.route('/run/move_absolute', methods=['GET', 'POST'])
+    @except_wrap
     def move_absolute():
-        @except_wrap
-        def wrap():
-            block = bool(request.args.get("block", default=True, type=int))
-            data = {}
-            for axis in "xyz":
-                this_pos = request.args.get("axis." + axis,
-                                            default=None,
-                                            type=float)
-                if this_pos is not None:
-                    data[axis] = this_pos
-            plugin.move_absolute(data, block=block)
-            return {'status': HTTPStatus.OK}
-
-        return wrap()
+        block = bool(request.args.get("block", default=True, type=int))
+        data = {}
+        for axis in "xyz":
+            this_pos = request.args.get("axis." + axis,
+                                        default=None,
+                                        type=float)
+            if this_pos is not None:
+                data[axis] = this_pos
+        plugin.move_absolute(data, block=block)
+        return {'status': HTTPStatus.OK}
 
     @app.route('/run/move_relative', methods=['GET', 'POST'])
+    @except_wrap
     def move_relative():
-        @except_wrap
-        def wrap():
-            block = bool(request.args.get("block", default=True, type=int))
-            data = {}
-            for axis in "xyz":
-                this_pos = request.args.get("axis." + axis,
-                                            default=None,
-                                            type=float)
-                if this_pos is not None:
-                    data[axis] = this_pos
-            plugin.move_relative(data, block=block)
-            return {'status': HTTPStatus.OK}
-
-        return wrap()
+        block = bool(request.args.get("block", default=True, type=int))
+        data = {}
+        for axis in "xyz":
+            this_pos = request.args.get("axis." + axis,
+                                        default=None,
+                                        type=float)
+            if this_pos is not None:
+                data[axis] = this_pos
+        plugin.move_relative(data, block=block)
+        return {'status': HTTPStatus.OK}
 
     @app.route('/get/system_status', methods=['GET'])
+    @except_wrap
     def system_status():
-        @except_wrap
-        def wrap():
-            system_status = plugin.system_status()
-            return {
-                'data': system_status,
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        system_status = plugin.system_status()
+        return {
+            'data': system_status,
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/get/is_idle', methods=['GET'])
+    @except_wrap
     def is_idle():
-        @except_wrap
-        def wrap():
-            is_idle = plugin.is_idle()
-            return {
-                'data': is_idle,
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        is_idle = plugin.is_idle()
+        return {
+            'data': is_idle,
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/get/pyuscope_version', methods=['GET'])
+    @except_wrap
     def pyuscope_version():
-        @except_wrap
-        def wrap():
-            pyuscope_version = plugin.pyuscope_version()
-            return {
-                'data': pyuscope_version,
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        pyuscope_version = plugin.pyuscope_version()
+        return {
+            'data': pyuscope_version,
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/get/objectives', methods=['GET'])
+    @except_wrap
     def objectives():
-        @except_wrap
-        def wrap():
-            objectives = plugin.get_objectives_config()
-            return {
-                'data': objectives,
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        objectives = plugin.get_objectives_config()
+        return {
+            'data': objectives,
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/get/active_objective', methods=['GET'])
+    @except_wrap
     def active_objective():
-        @except_wrap
-        def wrap():
-            objective = plugin.get_active_objective()
-            return {
-                'data': objective,
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        objective = plugin.get_active_objective()
+        return {
+            'data': objective,
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/set/active_objective/<objective>', methods=['GET', 'POST'])
+    @except_wrap
     def active_objective_set(objective):
-        @except_wrap
-        def wrap():
-            # Validate objective name before sending request
-            if objective not in plugin.objectives.names():
-                plugin.log(f"WARNING: objective '{objective}' not found")
-                return {'status': HTTPStatus.BAD_REQUEST}
-            plugin.set_active_objective(objective)
-            return {'status': HTTPStatus.OK}
-
-        return wrap()
+        # Validate objective name before sending request
+        if objective not in plugin.objectives.names():
+            plugin.log(f"WARNING: objective '{objective}' not found")
+            return {'status': HTTPStatus.BAD_REQUEST}
+        plugin.set_active_objective(objective)
+        return {'status': HTTPStatus.OK}
 
     @app.route('/get/imager/imager/get_disp_properties', methods=['GET'])
+    @except_wrap
     def imager_get_disp_properties():
-        @except_wrap
-        def wrap():
-            properties = plugin.imager_get_disp_properties()
-            return {
-                'data': properties,
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        properties = plugin.imager_get_disp_properties()
+        return {
+            'data': properties,
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/get/imager/disp_property/<name>', methods=['GET'])
+    @except_wrap
     def imager_get_disp_property(name):
-        @except_wrap
-        def wrap():
-            val = plugin.imager_get_disp_property(name)
-            return {
-                'data': val,
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        val = plugin.imager_get_disp_property(name)
+        return {
+            'data': val,
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/set/imager/disp_properties', methods=['GET', 'POST'])
+    @except_wrap
     def imager_set_disp_properties():
-        @except_wrap
-        def wrap():
-            properties = {}
-            for k, v in request.args.items():
-                # FIXME: not everything is a float
-                properties[k] = float(v)
-            # Validate objective name before sending request
-            plugin.imager_set_disp_properties(properties)
-            return {'status': HTTPStatus.OK}
-
-        return wrap()
+        properties = {}
+        for k, v in request.args.items():
+            # FIXME: not everything is a float
+            properties[k] = float(v)
+        # Validate objective name before sending request
+        plugin.imager_set_disp_properties(properties)
+        return {'status': HTTPStatus.OK}
 
     @app.route('/run/autofocus', methods=['GET', 'POST'])
+    @except_wrap
     def autofocus():
-        @except_wrap
-        def wrap():
-            block = bool(request.args.get("block", default=True, type=int))
-            plugin.autofocus(block=block)
-            return {'status': HTTPStatus.OK}
-
-        return wrap()
+        block = bool(request.args.get("block", default=True, type=int))
+        plugin.autofocus(block=block)
+        return {'status': HTTPStatus.OK}
 
     @app.route('/get/subsystem_functions', methods=['GET'])
+    @except_wrap
     def subsystem_functions():
-        @except_wrap
-        def wrap():
-            return {
-                'data': plugin.subsystem_functions_serialized(),
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        return {
+            'data': plugin.subsystem_functions_serialized(),
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/run/subsystem/<subsystem>/<function>',
                methods=['GET', 'POST'])
+    @except_wrap
     def subsystem_function(subsystem, function):
-        @except_wrap
-        def wrap():
-            kwargs = dict(request.args.items())
-            print("debug", subsystem, function, kwargs)
-            plugin.subsystem_function_serialized(subsystem_=subsystem,
-                                                 function_=function,
-                                                 **kwargs)
-            return {'status': HTTPStatus.OK}
-
-        return wrap()
+        kwargs = dict(request.args.items())
+        print("debug", subsystem, function, kwargs)
+        plugin.subsystem_function_serialized(subsystem_=subsystem,
+                                             function_=function,
+                                             **kwargs)
+        return {'status': HTTPStatus.OK}
 
     @app.route('/run/wait_imaging_ok', methods=['GET'])
+    @except_wrap
     def wait_imaging_ok():
-        @except_wrap
-        def wrap():
-            plugin.wait_imaging_ok()
-            return {
-                'status': HTTPStatus.OK,
-            }
-
-        return wrap()
+        plugin.wait_imaging_ok()
+        return {
+            'status': HTTPStatus.OK,
+        }
 
     @app.route('/get/image', methods=['GET'])
+    @except_wrap
     def get_image():
-        @except_wrap
-        def wrap():
-            wait_imaging_ok = bool(
-                request.args.get("wait_imaging_ok", default=True, type=int))
-            raw = bool(request.args.get("raw", default=False, type=int))
-            format_ = request.args.get("format", default="JPEG", type=str)
-            pil_image = plugin.image(wait_imaging_ok=wait_imaging_ok, raw=raw)
-            buffered = BytesIO()
-            pil_image.save(buffered, format=format_)
-            img_str = base64.b64encode(buffered.getvalue()).decode('ascii')
-            return {
-                'status': HTTPStatus.OK,
-                'data': {
-                    "format": format_,
-                    "base64": img_str,
-                }
+        wait_imaging_ok = bool(
+            request.args.get("wait_imaging_ok", default=True, type=int))
+        raw = bool(request.args.get("raw", default=False, type=int))
+        format_ = request.args.get("format", default="JPEG", type=str)
+        pil_image = plugin.image(wait_imaging_ok=wait_imaging_ok, raw=raw)
+        buffered = BytesIO()
+        pil_image.save(buffered, format=format_)
+        img_str = base64.b64encode(buffered.getvalue()).decode('ascii')
+        return {
+            'status': HTTPStatus.OK,
+            'data': {
+                "format": format_,
+                "base64": img_str,
             }
-
-        return wrap()
+        }
